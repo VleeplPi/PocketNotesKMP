3d63df7ab392bedd69b87661c95504b29f376ee2
VleeplPi <vlimaster@yandex.ru>
2024-06-16 10:55:04 +0700

    added theme

---
+++ composeApp/src/androidMain/kotlin/ru/pocket_notes/presentation/screen/home/HomeScreen.kt
@@ -0,0 +1,12 @@
+package ru.pocket_notes.presentation.screen.home
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+class HomeScreen: Screen {
+
+    @Composable
+    override fun Content(){
+    }
+
+}
